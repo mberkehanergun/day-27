0c2b7e946939233352c1bf6c58fd7df65eb43eac
mberkehanergun <140403112+mberkehanergun@users.noreply.github.com>
2023-08-16 20:49:50 +0300

    Update Main.java

--- application/Main.java
+++ application/Main.java
@@ -752,10 +752,11 @@ public class Main extends Application {
                         Label resultLabel = new Label("Results will show up here.");
                         TextArea taskTextArea = new TextArea();
                         Button loadReportButton = new Button("Load Report");
+                        Button backButtonToButtonScene = new Button("Back");
                         
                         VBox taskLayout = new VBox(10);
                         taskLayout.setAlignment(Pos.CENTER);
-                        taskLayout.getChildren().addAll(taskTextArea, loadReportButton, resultLabel, backButton);
+                        taskLayout.getChildren().addAll(taskTextArea, loadReportButton, resultLabel, backButtonToButtonScene);
 
                         Scene taskScene = new Scene(taskLayout, 600, 500);
                         primaryStage.setScene(taskScene);
@@ -767,6 +768,10 @@ public class Main extends Application {
                             }
                         	resultLabel.setText("Report loaded succesfully.");
                         	loadReportButton.setDisable(true);
+                        });
+                        
+                        backButtonToButtonScene.setOnAction(backToButtonEvent -> {
+                            primaryStage.setScene(buttonScene); // Switch to the "Button" scene
                         });
                     });
                     
